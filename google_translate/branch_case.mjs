4bc58337efda478289c7fbd0eef8281801f9275f
uno9 <ws060m@gmail.com>
2022-01-20 23:56:04 +0900

    [change] created-a-kebab-case

--- google_translate/branch_case.mjs
+++ google_translate/branch_case.mjs
@@ -3,7 +3,8 @@ export function camel(json) {
 }
 
 export function kebab(json) {
-
+  var lower_case = json.text.toLowerCase();
+  return lower_case.replace(/\s+/g,'-');
 }
 
 export function snake(json) {
